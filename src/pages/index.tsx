8357bc345a49f8de3e659d07c75bddd5916db68a
Macbook Pro 2017 <>
2023-03-30 23:23:33 +0700

    add react icons package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Button from "@/components/Button";
 import Head from "next/head";
+import { AiOutlineArrowRight, AiOutlineMail } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -11,6 +13,17 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-lg text-red-500">Hello</h1>
+        <div className="flex flex-row gap-4 items-center">
+          <Button>Hello</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
+          <Button disabled>Disabled</Button>
+          <Button isLoading>Loading Button</Button>
+          <Button leftIcon={<AiOutlineMail />} variant="link">
+            Email
+          </Button>
+          <Button rightIcon={<AiOutlineArrowRight />}>Continue</Button>
+        </div>
       </main>
     </>
   );

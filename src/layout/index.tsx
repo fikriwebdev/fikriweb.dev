c8f8977bbc52c6507026f6de9944a8898e0006a8
Macbook Pro 2017 <>
2023-04-30 18:07:22 +0700

    Add: text gradient in hero

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: PropsWithChildren) {
       <Header />
       <div className="flex-1 container mx-auto">
         <main className="min-h-screen mt-[var(--header-height)] container max-w-7xl mx-auto px-0 md:px-4 mb-56">
-          <div className="pt-4 flex flex-col">{children}</div>
+          <div className="pt-8 flex flex-col">{children}</div>
         </main>
       </div>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,16 +13,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <section className="relative flex flex-col items-center justify-center gap-4 paper h-72">
+        <section className="relative flex flex-col items-center justify-center gap-2 paper">
           <Avatar
             src="/assets/images/avatar-hero.png"
             size="xl"
             className="shadow-lg"
           />
-          <h1 className="text-4xl font-medium">Hello, I&apos;m Fikri 👋</h1>
-          <p className="text-gray-500 text-lg text-center">
-            I like to create front-end solutions that are fast, flexible, and
-            flawless.
+          <h1 className="text-base text-gray-500 font-medium">
+            Hello, I&apos;m Fikri 👋
+          </h1>
+          <p className="text-4xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-black dark:from-white to-gray-300  dark:to-gray-700 text-center">
+            I like to create front-end solutions <br className="" /> that are
+            fast, flexible,
+            <br className="" /> and flawless.
           </p>
         </section>
       </Layout>

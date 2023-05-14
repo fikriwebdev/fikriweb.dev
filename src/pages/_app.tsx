6c7e9158890fa0894511dfda1f01eb77533b3dc2
Macbook Pro 2017 <>
2023-05-14 22:13:24 +0700

    feature: code block for blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { MDXProvider } from "@mdx-js/react";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import defaultSeo from "../../next-seo.config";
+import "../styles/code-highlight.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,9 +1,12 @@
+import Heading from "@/components/Heading";
 import Layout from "@/layout";
 import { getAllPosts, getPostBySlug } from "@/libs/mdx";
 import { IBlog } from "@/types";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
+import Image from "next/image";
+import rehypePrism from "rehype-prism-plus";
 
 interface BlogDetailsProps {
     source: MDXRemoteSerializeResult<
@@ -14,13 +17,31 @@ interface BlogDetailsProps {
 }
 
 export default function BlogDetails({ source, data }: BlogDetailsProps) {
+    console.log(source);
     return (
         <Layout
             seo={{
                 title: data.title,
             }}
         >
-            <div className="container mx-auto max-w-lg px-4 md:px-0 blog">
+            <div className="container mx-auto max-w-full md:max-w-4xl px-4 md:px-0 blog">
+                <div className="mb-4 border-b pb-4 border-gray-100 dark:border-gray-700">
+                    <div className="relative w-full h-[200px] md:h-[400px] rounded-md overflow-hidden mb-4">
+                        <Image
+                            src={data.banner}
+                            alt={data.title}
+                            fill
+                            style={{
+                                objectFit: "cover",
+                                objectPosition: "center",
+                            }}
+                        />
+                    </div>
+                    <Heading as="h1">{data.title}</Heading>
+                    <p className="text-gray-800 dark:text-gray-400 text-sm md:text-base">
+                        {data.publishedAt}
+                    </p>
+                </div>
                 <MDXRemote {...source} />
             </div>
         </Layout>
@@ -42,7 +63,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     const { content, data } = getPostBySlug(
         typeof params?.slug === "string" ? params.slug : ""
     );
-    const mdxSource = await serialize(content);
+    const mdxSource = await serialize(content, {
+        mdxOptions: {
+            rehypePlugins: [rehypePrism],
+        },
+    });
 
     return {
         props: {

61580ddede14887a3676490edd155bf6a7a11063
Macbook Pro 2017 <>
2023-05-15 22:36:49 +0700

    add: serialize mdx content directly on getAllContents & getContentBySlug

--- libs/mdx.ts
+++ libs/mdx.ts
@@ -1,25 +1,41 @@
-import path from "path";
 import fs from "fs";
 import matter, { GrayMatterFile } from "gray-matter";
-import { IBlog } from "@/types";
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import path from "path";
+import rehypePrism from "rehype-prism-plus";
 
-export const getContentBySlug = (folder: string, slug: string) => {
+export const getContentBySlug = async <T>(folder: string, slug: string) => {
     const filePath = path.join(process.cwd(), "content", folder, `${slug}.mdx`);
     const file = fs.readFileSync(filePath);
-    const content = matter(file);
+    const data = matter(file);
 
-    return content as GrayMatterFile<Buffer> & { data: IBlog };
-};
-
-export const getAllContent = (folder: string) => {
-    const filePath = path.join(process.cwd(), "content", folder);
-    const files = fs.readdirSync(filePath);
-    const content = files.map(file => {
-        const filename = file.replace(".mdx", "");
-        const { data } = getContentBySlug(folder, filename);
-
-        return { ...data, slug: filename };
+    const source = await serialize(data.content, {
+        mdxOptions: {
+            rehypePlugins: [rehypePrism],
+        },
     });
 
-    return content as unknown as IBlog[];
+    return { ...data, source } as GrayMatterFile<Buffer> & {
+        data: T;
+        source: MDXRemoteSerializeResult<
+            Record<string, unknown>,
+            Record<string, unknown>
+        >;
+    };
+};
+
+export const getAllContent = async <T>(folder: string) => {
+    const filePath = path.join(process.cwd(), "content", folder);
+    const files = fs.readdirSync(filePath);
+    const content = await Promise.all(
+        files.map(async file => {
+            const filename = file.replace(".mdx", "");
+            const { data, source } = await getContentBySlug(folder, filename);
+
+            return { ...data, source, slug: filename };
+        })
+    );
+
+    return content as unknown as T;
 };

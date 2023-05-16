7645894b8233cc5c30d864368e38dcac31582764
Macbook Pro 2017 <>
2023-05-16 21:33:41 +0700

    fix:project card styling both on mobile and desktop view

--- components/shared/ProjectCard.tsx
+++ components/shared/ProjectCard.tsx
@@ -8,7 +8,7 @@ import { SiNextdotjs } from "react-icons/si";
 
 const OverlapedImages = ({ images }: { images: string[] }) => {
     return (
-        <div className="w-full p-4 relative col-span-4   ml-4 md:ml-0">
+        <div className="w-full p-4 relative col-span-4 mt-16 md:mt-0   ml-4 md:ml-0 block  md:flex items-center md:mb-6">
             <div className="absolute">
                 <div className="w-[12rem] h-[12rem] relative rounded-md overflow-hidden transition-transform duration-500 transform rotate-[-4deg] group-hover:-translate-x-4 group-hover:scale-105 group-hover:rotate-[-10deg]">
                     <Image
@@ -48,8 +48,8 @@ export default function ProjectCard({ data }: { data: IProject }) {
 
     return (
         <Link href={`/projects/${data.slug}`}>
-            <div className="w-full h-[30rem] md:h-[15rem] bg-white dark:bg-gray-900 group   border border-gray-300 dark:border-gray-800 rounded-md overflow-hidden relative   bg-transparent grid grid-cols-1 md:grid-cols-7 content-start justify-center place-content-center md:content-center paper-project-card hover:border-gray-500 dark:hover:border-gray-700 transition-colors duration-500">
-                <div className="p-4 col-span-3">
+            <div className="w-full h-[25rem] md:h-[18rem] bg-white dark:bg-gray-900 group   border border-gray-300 dark:border-gray-800 rounded-md overflow-hidden relative   bg-transparent flex flex-col md:grid  md:grid-cols-7  paper-project-card hover:border-gray-500 dark:hover:border-gray-700 transition-colors duration-500">
+                <div className="px-4 col-span-3 h-[80px]  md:h-full block pt-4 md:pt-0 md:flex flex-col justify-center">
                     <div className="flex items-center gap-4 mb-2 text-xl text-gray-700 dark:text-gray-200 ">
                         <FaReact />
                         <SiNextdotjs />
@@ -58,11 +58,10 @@ export default function ProjectCard({ data }: { data: IProject }) {
                         {data.title}
                     </h1>
 
-                    <p className="text-gray-700 dark:text-gray-200 text-sm mt-2 line-clamp-3">
-                        {data.description}
+                    <p className="text-gray-700 dark:text-gray-200 text-sm mt-2 line-clamp-5">
+                        {data.description} Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Officia, aliquid.
                     </p>
-
-                    <div className="w-4 h-[48px]" />
                 </div>
                 <OverlapedImages images={images} />
             </div>

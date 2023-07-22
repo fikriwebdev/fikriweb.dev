5eee3482f681abcb46230d74a8922330d35f7d10
Macbook Pro 2017 <>
2023-07-23 02:41:12 +0700

    delete default open graph

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -7,20 +7,7 @@ const defaultSeo: DefaultSeoProps = {
         "I like to create front-end solutions  that are fast, flexible",
     titleTemplate: "fikriweb.dev | %s ",
     canonical: `${process.env.PROD_URL}`,
-    openGraph: {
-        url: `${process.env.PROD_URL}`,
-        title: "fikriweb.dev",
-        description:
-            "I like to create front-end solutions  that are fast, flexible",
-        images: [
-            {
-                url: "/assets/images/logo.png",
-                width: 800,
-                height: 600,
-                alt: "fikriweb.dev logo",
-            },
-        ],
-    },
+
     twitter: {
         cardType: "summary",
         site: "@fikriweb.dev",
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -5,6 +5,7 @@ import Hero from "./components/HomeHero";
 import Projects from "./components/Projects";
 import Timelines from "./components/Timelines";
 import UIRecipes from "./components/UIRecipes";
+import { NextSeo } from "next-seo";
 
 interface HomeProps {
     blogs: BlogType[];
@@ -15,6 +16,22 @@ interface HomeProps {
 export default function ViewHome({ blogs, projects, recipes }: HomeProps) {
     return (
         <>
+            <NextSeo
+                openGraph={{
+                    url: `${process.env.PROD_URL}`,
+                    title: "fikriweb.dev",
+                    description:
+                        "I like to create front-end solutions  that are fast, flexible",
+                    images: [
+                        {
+                            url: "/assets/images/logo.png",
+                            width: 800,
+                            height: 600,
+                            alt: "fikriweb.dev logo",
+                        },
+                    ],
+                }}
+            />
             <Hero />
             <Timelines />
             <Blog data={blogs} />

a6dbce39fdb428730d6a902dc285d3772ecc5393
Macbook Pro 2017 <>
2023-07-24 00:02:09 +0700

    remove default canonical and og on next-seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,22 +3,23 @@ import { DefaultSeoProps } from "next-seo";
 const defaultSeo: DefaultSeoProps = {
     title: undefined,
     defaultTitle: "fikriweb.dev",
-    description:
-        "I like to create front-end solutions  that are fast, flexible",
     titleTemplate: "fikriweb.dev | %s ",
-    canonical: `${process.env.NEXT_PUBLIC_PROD_URL}`,
-    openGraph: {
-        url: `${process.env.NEXT_PUBLIC_PROD_URL}`,
-        title: "fikriweb.dev",
-        description:
-            "I like to create front-end solutions  that are fast, flexible",
-        images: [
-            {
-                url: "/assets/images/logo.png",
-                alt: "fikriweb.dev logo",
-            },
-        ],
-    },
+    // description:
+    //     "I like to create front-end solutions  that are fast, flexible",
+
+    // canonical: `${process.env.NEXT_PUBLIC_PROD_URL}`,
+    // openGraph: {
+    //     url: `${process.env.NEXT_PUBLIC_PROD_URL}`,
+    //     title: "fikriweb.dev",
+    //     description:
+    //         "I like to create front-end solutions  that are fast, flexible",
+    //     images: [
+    //         {
+    //             url: "/assets/images/logo.png",
+    //             alt: "fikriweb.dev logo",
+    //         },
+    //     ],
+    // },
     twitter: {
         cardType: "summary",
         site: "@fikriweb.dev",
